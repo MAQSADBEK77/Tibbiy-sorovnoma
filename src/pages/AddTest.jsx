5c967e7ce5c8a3460923e495bbaa5316de6babb6
Maqsadbek <110594640+MAQSADBEK77@users.noreply.github.com>
2024-01-14 08:02:04 +0500

    Install AddTest component

--- src/pages/AddTest.jsx
+++ src/pages/AddTest.jsx
@@ -5,29 +5,40 @@ function AddTest() {
       <h1 className="text-4xl text-center mt-10">Yangi test qo'shish</h1>
       <MiniSelect
         data={[
-          { name: "Bosh kasalliklari test natijalari", value: "bosh" },
-          { name: "Oyoq kasalliklari test natijalari", value: "bosh" },
-          { name: "Ichak kasalliklari test natijalari", value: "bosh" },
+          { name: "Tibbiy-Ijtimoiy so'ro'vnoma(Ayollar uchun)", value: "1" },
         ]}
       />
       <form className="flex flex-col gap-y-8">
         <input
           type="text"
           placeholder="Test savolini kiriting"
+          className="input input-bordered input-warning w-full"
+        />
+        <input
+          type="text"
+          placeholder="A) variantni kiriting"
           className="input input-bordered input-success w-full"
         />
-        <div className="w-full flex items-center">
-          <input
-            type="checkbox"
-            checked="checked"
-            className="checkbox checkbox-success"
-          />
-          <input
-            type="text"
-            placeholder="Test savolini kiriting"
-            className="input input-bordered input-success w-full"
-          />
-        </div>
+        <input
+          type="text"
+          placeholder="B) variantni kiriting"
+          className="input input-bordered input-success w-full"
+        />{" "}
+        <input
+          type="text"
+          placeholder="D) variantni kiriting"
+          className="input input-bordered input-success w-full"
+        />{" "}
+        <input
+          type="text"
+          placeholder="E) variantni kiriting"
+          className="input input-bordered input-success w-full"
+        />{" "}
+        <input
+          type="text"
+          placeholder="F) variantni kiriting"
+          className="input input-bordered input-success w-full"
+        />
       </form>
     </div>
   );
